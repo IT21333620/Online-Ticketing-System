d388216ae652ac6eceb8e31d52adb1c07e1e37c1
Kavija Sapukotana <it21333620@my.sliit.lk>
2023-10-13 20:39:16 +0530

    init commit

---
+++ src/main/java/com/example/onlineticketingsystem/OnlineTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineTicketingSystemApplication.class, args);
+    }
+
+}
