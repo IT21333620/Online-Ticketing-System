524980c9a158f14c757fcb54f55f5dbd3f7599fa
Shadhir Ameen <99326335+IT21309038@users.noreply.github.com>
2023-10-17 21:07:44 +0530

    singleton pattern

--- src/main/java/com/example/onlineticketingsystem/controller/PassengerController.java
+++ src/main/java/com/example/onlineticketingsystem/controller/PassengerController.java
@@ -1,29 +1,53 @@
 package com.example.onlineticketingsystem.controller;
 
 import com.example.onlineticketingsystem.DTO.PassengerDTO;
+import com.example.onlineticketingsystem.DatabaseConnectionManager;
 import com.example.onlineticketingsystem.service.PassengerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @RestController
 @RequestMapping(value = "api/tickets")
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin
 public class PassengerController {
+
+    private DatabaseConnectionManager connectionManager = DatabaseConnectionManager.getInstance();
 
     @Autowired
     private PassengerService passengerService;
 
     //insert passenger
     @PostMapping("/createPassenger")
-    public PassengerDTO savePassenger (@RequestBody PassengerDTO passengerDTO){
-        return passengerService.savePassenger(passengerDTO);
+    public PassengerDTO savePassenger(@RequestBody PassengerDTO passengerDTO) {
+        try (Connection connection = connectionManager.getConnection()) {
+            // Use the 'connection' to perform database operations
+            passengerService.savePassenger(passengerDTO);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle database connection or operation error
+            // You might want to return an appropriate HTTP response in case of an error.
+        }
+
+        // Return a response indicating success
+        return passengerDTO;
     }
 
     //show all feilds of passenger
     @GetMapping("/passengerById/{userID}")
-    public PassengerDTO getPassengerById(@PathVariable int userID){
-        return passengerService.getPassengerById(userID);
+    public PassengerDTO getPassengerById(@PathVariable int userID) {
+        try (Connection connection = connectionManager.getConnection()) {
+            // Use the 'connection' to perform database operations
+            return passengerService.getPassengerById(userID);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle database connection or operation error
+            // You might want to return an appropriate HTTP response in case of an error.
+            return null; // or throw an exception, or handle the error as per your application's requirements.
+        }
     }
 
     //show balance of passenger
